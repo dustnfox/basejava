c900353e64b9a0e839ff9f8be859688874e2b963
Mikhail Rassadin <dustnfox@gmail.com>
2017-11-28 10:52:51 +0300

    Add Reflection test
    Add class with java reflection mechanism examples.
    Add invoking toString() method of the Resume class through reflection.

---
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -0,0 +1,22 @@
+package ru.javawebinar.basejava;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Resume r = new Resume();
+        Field field = r.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(r));
+        field.set(r, "new_uuid");
+        // Invoking toString from Resume object via reflection
+        Method toStringResumeMethod = r.getClass().getMethod("toString");
+        String toStringResult = (String) toStringResumeMethod.invoke(r);
+        System.out.println(toStringResult);
+    }
+}
