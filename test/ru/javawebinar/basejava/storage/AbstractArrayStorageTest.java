feb6d56c52a594cc474c02fbccd59254d598e71e
Mikhail Rassadin <dustnfox@gmail.com>
2017-11-29 11:40:01 +0300

    Change JUnit tests
    Add tests for order violation for SortedArrayStorage.
    Fix sizeOverflow test. Check if doesn't throw exception until overflow.
    Check exception throwing right after overflow occurs.
    Declare test Resume objects as final static.

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -12,10 +12,16 @@ import ru.javawebinar.basejava.model.Resume;
 import static org.junit.Assert.*;
 
 public abstract class AbstractArrayStorageTest {
-    private Storage storage;
-    private static final String UUID_1 = "uuid1";
-    private static final String UUID_2 = "uuid2";
-    private static final String UUID_3 = "uuid3";
+    final Storage storage;
+    static final String UUID_1 = "uuid1";
+    static final String UUID_2 = "uuid2";
+    static final String UUID_3 = "uuid3";
+    static final String UUID_4 = "uuid4";
+    static final String UUID_NON_EXISTS = "dummy";
+    static final Resume RESUME_1 = new Resume(UUID_1);
+    static final Resume RESUME_2 = new Resume(UUID_2);
+    static final Resume RESUME_3 = new Resume(UUID_3);
+    static final Resume RESUME_4 = new Resume(UUID_4);
 
     AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
@@ -23,9 +29,9 @@ public abstract class AbstractArrayStorageTest {
 
     @Before
     public void setUp() throws Exception {
-        storage.save(new Resume(UUID_1));
-        storage.save(new Resume(UUID_2));
-        storage.save(new Resume(UUID_3));
+        storage.save(RESUME_1);
+        storage.save(RESUME_2);
+        storage.save(RESUME_3);
     }
 
     @After
@@ -48,30 +54,29 @@ public abstract class AbstractArrayStorageTest {
 
     @Test(expected = NotExistStorageException.class)
     public void updateNotExist() throws Exception {
-        storage.update(new Resume("dummy"));
+        storage.update(new Resume(UUID_NON_EXISTS));
     }
 
     @Test
     public void save() throws Exception {
-        storage.save(new Resume("UUID_4"));
+        storage.save(RESUME_4);
         assertEquals(4, storage.size());
-        storage.get("UUID_4");
+        storage.get(UUID_4);
     }
 
     @Test(expected = ExistStorageException.class)
     public void saveExist() throws ExistStorageException {
-        storage.save(new Resume(UUID_1));
+        storage.save(RESUME_1);
     }
 
     @Test
     public void get() throws Exception {
-        Resume rTest = storage.get(UUID_1);
-        assertEquals(UUID_1, rTest.getUuid());
+        assertEquals(UUID_1, RESUME_1.getUuid());
     }
 
     @Test(expected = NotExistStorageException.class)
     public void getNotExist() throws Exception {
-        storage.get("dummy");
+        storage.get(UUID_NON_EXISTS);
     }
 
     @Test
@@ -84,7 +89,7 @@ public abstract class AbstractArrayStorageTest {
 
     @Test(expected = NotExistStorageException.class)
     public void deleteNotExist() throws Exception {
-        storage.delete("dummy");
+        storage.delete(UUID_NON_EXISTS);
     }
 
     @Test
@@ -103,8 +108,15 @@ public abstract class AbstractArrayStorageTest {
 
     @Test(expected = StorageException.class)
     public void sizeOverflow() throws Exception {
-        for(int i = 0; i < 100001 - 3; i++) {
-            storage.save(new Resume(String.valueOf(i)));
+        storage.clear();
+        try {
+            for (int i = 0; i < 10000; i++) {
+                storage.save(new Resume(String.valueOf(i)));
+            }
+        } catch (StorageException e) {
+            fail();
         }
+
+        storage.save(new Resume("10001"));
     }
 }
--- test/ru/javawebinar/basejava/storage/SortedArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/SortedArrayStorageTest.java
@@ -1,8 +1,69 @@
 package ru.javawebinar.basejava.storage;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import ru.javawebinar.basejava.model.Resume;
+
+import java.util.Arrays;
+
 
 public class SortedArrayStorageTest extends AbstractArrayStorageTest{
     public SortedArrayStorageTest() {
         super(new SortedArrayStorage());
     }
+
+    @Test
+    public void saveOrder() throws Exception {
+        final Resume r0 = new Resume("uuid0");
+        final Resume r5 = new Resume("uuid5");
+
+        storage.save(r0); // Save to the left end.
+        storage.save(r5); // Save to the right end.
+        storage.save(RESUME_4); // Save in the middle.
+
+        Resume[] getAllArray = storage.getAll();
+        Resume[] expectedArray = {
+                r0,
+                RESUME_1,
+                RESUME_2,
+                RESUME_3,
+                RESUME_4,
+                r5
+        };
+        assertTrue(Arrays.equals(expectedArray, getAllArray));
+    }
+
+    @Test
+    public void deleteOrder() throws Exception {
+        storage.save(RESUME_4);
+
+        // Delete from the left end.
+        storage.delete(UUID_1);
+        Resume[] getAllArray = storage.getAll();
+        Resume[] expectedArray = {
+                RESUME_2,
+                RESUME_3,
+                RESUME_4
+        };
+        assertTrue(Arrays.equals(expectedArray, getAllArray));
+
+        //Delete from the middle
+        storage.delete(UUID_3);
+        getAllArray = storage.getAll();
+        expectedArray = new Resume[]{
+                RESUME_2,
+                RESUME_4
+        };
+        assertTrue(Arrays.equals(expectedArray, getAllArray));
+
+        //Delete from the right end
+        storage.delete(UUID_4);
+        getAllArray = storage.getAll();
+        expectedArray = new Resume[]{
+                RESUME_2
+        };
+        assertTrue(Arrays.equals(expectedArray, getAllArray));
+    }
+
 }
